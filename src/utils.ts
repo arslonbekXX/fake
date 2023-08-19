b208efac25e6794a999af4648ec3eea05bded432
arslonbekXX <arslonbek.lion067@gmail.com>
2023-08-19 09:29:10 +0500

    lesson-69 start-codes

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export const random = (max: number, min = 0) => {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-export const randomColor = () => {
-	return `#${Math.random().toString(16).substring(2, 8)}`;
-};
